85639ba7d9aee13f45c12220c5a700fe7565a4ba
Harith <2022807912@student.uitm.edu.my>
2026-07-06 19:54:57 +0800

    Fix Laravel logging for Vercel read-only filesystem

--- config/logging.php
+++ config/logging.php
@@ -13,10 +13,10 @@ return [
     ],
     'channels' => [
         'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
-            'ignore_exceptions' => false,
-        ],
+    'driver' => 'stack',
+    'channels' => ['stderr'],
+    'ignore_exceptions' => false,
+],
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
